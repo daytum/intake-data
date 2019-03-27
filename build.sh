10b417f687df74ec05e9c0386ab272c5f3c4c17b
John T. Foster <johntfosterjr@gmail.com>
2019-03-27 08:24:04 -0500

    Remove conda build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p $PREFIX/share/intake/data
-cp $SRC_DIR/data/*_production.csv $PREFIX/share/intake/data
-cp $RECIPE_DIR/production.yml $PREFIX/share/intake/
-cp $RECIPE_DIR/csv.yml $PREFIX/share/intake/
