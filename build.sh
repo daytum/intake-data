798ee3a7b62c4551aad5cf5974507450f3d77032
John T. Foster <johntfosterjr@gmail.com>
2019-06-23 23:08:58 -0500

    Add 2D_MV_200wells data from Pyrcz

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p $PREFIX/share/intake/data
 cp $RECIPE_DIR/data/poro_perm.csv $PREFIX/share/intake/data/
+cp $RECIPE_DIR/data/2D_MV_200wells.csv $PREFIX/share/intake/data/
 cp $RECIPE_DIR/us_production.yml $PREFIX/share/intake/
 cp $RECIPE_DIR/us_wells.yml $PREFIX/share/intake/
 cp $RECIPE_DIR/petrophysical.yml $PREFIX/share/intake/
