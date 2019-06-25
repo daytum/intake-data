3053263974f6fac83e6029fec58aa256552ce0f2
John T. Foster <johntfosterjr@gmail.com>
2019-06-25 18:23:43 -0500

    Add unconv_MV

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 mkdir -p $PREFIX/share/intake/data
 cp $RECIPE_DIR/data/poro_perm.csv $PREFIX/share/intake/data/
 cp $RECIPE_DIR/data/2D_MV_200wells.csv $PREFIX/share/intake/data/
+cp $RECIPE_DIR/data/unconv_MV.csv $PREFIX/share/intake/data/
 cp $RECIPE_DIR/us_production.yml $PREFIX/share/intake/
 cp $RECIPE_DIR/us_wells.yml $PREFIX/share/intake/
 cp $RECIPE_DIR/petrophysical.yml $PREFIX/share/intake/
