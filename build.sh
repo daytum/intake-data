1042b901dd90a497b90b8b39703c6d47c3e59c75
John T. Foster <johntfosterjr@gmail.com>
2019-06-26 18:57:00 -0500

    Add 12_sample_data

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cp $RECIPE_DIR/data/2D_MV_200wells.csv $PREFIX/share/intake/data/
 cp $RECIPE_DIR/data/unconv_MV.csv $PREFIX/share/intake/data/
 cp $RECIPE_DIR/data/Density_Por_data.csv $PREFIX/share/intake/data/
 cp $RECIPE_DIR/data/sample_data_biased.csv $PREFIX/share/intake/data/
+cp $RECIPE_DIR/data/12_sample_data.csv $PREFIX/share/intake/data/
 cp $RECIPE_DIR/us_production.yml $PREFIX/share/intake/
 cp $RECIPE_DIR/us_wells.yml $PREFIX/share/intake/
 cp $RECIPE_DIR/petrophysical.yml $PREFIX/share/intake/
