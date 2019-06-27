6f0f6fc311263cdb1934ba61be346cc76770f07d
John T. Foster <johntfosterjr@gmail.com>
2019-06-27 11:26:39 -0500

    Add AI grid as Numpy Source

--- build.sh
+++ build.sh
@@ -13,3 +13,4 @@ cp $RECIPE_DIR/data/sample_data_12.csv $PREFIX/share/intake/data/
 cp $RECIPE_DIR/us_production.yml $PREFIX/share/intake/
 cp $RECIPE_DIR/us_wells.yml $PREFIX/share/intake/
 cp $RECIPE_DIR/petrophysical.yml $PREFIX/share/intake/
+cp $RECIPE_DIR/siesmic.yml $PREFIX/share/intake/
