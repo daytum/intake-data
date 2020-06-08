057718666f7af7bf68d893b47b011b71f44463a1
John T. Foster <johntfosterjr@gmail.com>
2020-06-08 16:37:33 -0500

    Fix bug for nonlinear_facies

--- daytum_cat/__init__.py
+++ daytum_cat/__init__.py
@@ -31,3 +31,5 @@ sample_data_biased = petrophysical.sample_data_biased
 sample_data_MV_biased = petrophysical.sample_data_MV_biased
 sample_data = petrophysical.sample_data
 porosity_1D = petrophysical.porosity_1D
+nonlinear_facies_v1 = petrophysical.nonlinear_facies_v1
+nonlinear_facies_v2 = petrophysical.nonlinear_facies_v2
