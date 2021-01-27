5cd23db1b20c892342a4b33eefa19c7772f661e2
John T. Foster <johntfosterjr@gmail.com>
2021-01-27 17:09:46 -0600

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='daytum-data',
-    version='0.2.9',
+    version='0.2.10',
     author="John T. Foster",
     author_email="john@daytum.io",
     description="Intake data package for Daytum classes",
