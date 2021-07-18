0ffca9372b644e7b481546cdeddd4127de84a62b
John T. Foster <johntfosterjr@gmail.com>
2021-07-18 10:58:35 -0500

    Fix GrainSize data

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='daytum-data',
-    version='0.2.12',
+    version='0.2.13',
     author="John T. Foster",
     author_email="john@daytum.io",
     description="Intake data package for Daytum classes",
