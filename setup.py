53d0ac67fcdf346b0f64ff695a58a98c73b32c24
John T. Foster <johntfosterjr@gmail.com>
2021-10-20 17:39:03 +0000

    Fix issue w/ allowed fields

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='daytum-data',
-    version='0.2.10',
+    version='0.2.11',
     author="John T. Foster",
     author_email="john@daytum.io",
     description="Intake data package for Daytum classes",
